3efe9719d6680db7eb9ba663c46efc58545505cb
diyanav <diyana.v20@gmail.com>
2023-06-22 20:43:26 +0300

    lab3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("QA Automation course.");
+        //System.out.println("QA Automation course.");
     }
+
 }
+
