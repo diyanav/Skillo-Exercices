0ccd9b606a710cdebcb32c9343765a0a037c5d42
diyanav <diyana.v20@gmail.com>
2023-06-27 11:59:22 +0300

    lab6 - Exercices: Exceptions

--- src/lab6/Exercices/Exceptions/FixCode.java
+++ src/lab6/Exercices/Exceptions/FixCode.java
@@ -7,21 +7,26 @@ import java.util.Scanner;
 public class FixCode {
     private static FileWriter getFileWriter(String filePath) throws IOException {
         FileWriter fileWriter = new FileWriter(filePath, true);
-        return fileWriter; }
+        return fileWriter;
+    }
 
     protected static void closeFileWriter(FileWriter fileWriter) throws IOException {
-        fileWriter.close(); }
+        fileWriter.close();
+    }
 
     public static void printToFile(FileWriter fileWriter, String text, int repeat) throws IOException {
         for (int i = 0; i < repeat; i++) {
             fileWriter.write(text + "\n");
         }
     }
+
     public static void main(String[] args) throws IOException {
         Scanner scanner = new Scanner(System.in);
+
         String filePath = scanner.nextLine();
         int howManyTimesToPrintTheTextToFile = scanner.nextInt();
         String textToAdd = scanner.nextLine();
+
         printToFile(getFileWriter(filePath), textToAdd, howManyTimesToPrintTheTextToFile);
     }
 }
